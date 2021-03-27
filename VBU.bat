72ecafd83452ad7ee4651e83c083605da0ac0cfd
baikil <78218286+baikil@users.noreply.github.com>
2021-03-27 00:32:31 -0400

    Add files via upload

--- VBU.bat
+++ VBU.bat
@@ -10,7 +10,19 @@ title %appn% v%appv% - %appv%
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/baikil/VBU/main/version.vbuf', 'version.vbuf')"
 for /f "tokens=*" %%A in (version.vbuf) do (set "version=%%A")
 if not %appv%==%version% goto update
+
 :verif2
+if exist settings.vbuf goto menu
+echo Choose language / Choisir la langue
+echo EN=English   FR=Francais
+echo.
+set /p lang=
+echo %lang%>settings.vbuf
+
+:menu
+type settings.vbuf
+pause
+exit
 
 :download
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%1', '%2')"
